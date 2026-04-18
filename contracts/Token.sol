50139d5b11462a5b0dfd72c35d42be362696b34a
Kaixinnnnnnnnnn <kai1005xin@gmail.com>
2026-04-18 09:22:38 +0800

    refund + withdraw

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -5,11 +5,27 @@ contract Token {
     string public name = "CrowdToken";
     string public symbol = "CTK";
     uint public totalSupply;
-
+    address public crowdfundingContract;
     mapping(address => uint) public balanceOf;
+
+    event Transfer(address indexed from, address indexed to, uint value);
+
+    function setCrowdfundingContract(address _addr) public {
+        require(crowdfundingContract == address(0), "Already set");
+        require(_addr != address(0), "Zero address");
+        crowdfundingContract = _addr;
+    }
 
     function mint(address to, uint amount) public {
         balanceOf[to] += amount;
         totalSupply += amount;
+    }
+
+    function transfer(address to, uint amount) public returns (bool) {
+        require(balanceOf[msg.sender] >= amount, "Insufficient balance");
+        balanceOf[msg.sender] -= amount;
+        balanceOf[to] += amount;
+        emit Transfer(msg.sender, to, amount);
+        return true;
     }
 }
--- contracts/crowdfunding.sol
+++ contracts/crowdfunding.sol
@@ -24,12 +24,31 @@ contract Crowdfunding {
 
     mapping(uint => mapping(address => uint)) public contributions;
     mapping(uint => address[]) public contributors;
+    mapping(uint => mapping(address => bool)) public tokensClaimed;
 
     event CampaignCreated(
         uint campaignId,
         address creator,
         uint goal,
         uint deadline
+    );
+
+    event Withdrawn(
+        uint campaignId, 
+        address creator, 
+        uint amount
+    );
+
+    event Refunded(
+        uint campaignId, 
+        address contributor,
+        uint amount
+    );
+
+    event TokensClaimed(
+        uint campaignId,
+        address contributor, 
+        uint amount
     );
 
     function createCampaign(
@@ -73,15 +92,53 @@ contract Crowdfunding {
         c.amountRaised += msg.value;
     }
 
-    // function withdraw(uint id) public {
+    function withdraw(uint id) public {
+        require(id < campaigns.length, "Campaign does not exist");
+        Campaign storage c = campaigns[id];
+        require(msg.sender == c.creator,       "Only creator can withdraw");
+        require(block.timestamp >= c.deadline, "Campaign still active");
+        require(c.amountRaised >= c.goal,      "Funding goal not reached");
+        require(!c.withdrawn,                  "Already withdrawn");
 
-    // }
+        c.withdrawn = true;
+        uint amount = c.amountRaised;
+        payable(c.creator).transfer(amount);
+        emit Withdrawn(id, c.creator, amount);
+    }
 
-    // function refund(uint id) public {
+    function refund(uint id) public {
+        require(id < campaigns.length, "Campaign does not exist");
+        Campaign storage c = campaigns[id];
+        require(block.timestamp > c.deadline, "Campaign not ended yet");
+        require(c.amountRaised < c.goal, "Goal was reached, no refund");
+        uint amount = contributions[id][msg.sender];
+        require(amount > 0, "No contribution to refund");
 
-    // }
+        contributions[id][msg.sender] = 0;
+        payable(msg.sender).transfer(amount);
+        emit Refunded(id, msg.sender, amount);
+    }
 
     function getCampaignCount() public view returns (uint) {
         return campaigns.length;
     }
+
+    function claimTokens(uint id) public {
+        require(id < campaigns.length, "Campaign does not exist");
+        Campaign storage c = campaigns[id];
+        require(c.amountRaised >= c.goal,          "Campaign did not succeed");
+        require(contributions[id][msg.sender] > 0, "No contribution found");
+        require(!tokensClaimed[id][msg.sender], "Tokens already claimed");
+        uint contributed  = contributions[id][msg.sender];
+        uint wholeEth     = contributed / 1 ether;
+        uint tokensToMint = wholeEth * 1e18;
+        require(tokensToMint > 0, "Need at least 1 ETH to earn tokens");
+        tokensClaimed[id][msg.sender] = true;
+        token.mint(msg.sender, tokensToMint);
+        emit TokensClaimed(id, msg.sender, tokensToMint);
+    }
+
+    function getContribution(uint id, address contributor) public view returns (uint) { return contributions[id][contributor]; }
+
+    function hasClaimedTokens(uint id, address contributor) public view returns (bool) { return tokensClaimed[id][contributor]; }
 }
